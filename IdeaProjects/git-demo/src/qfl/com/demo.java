a6b1fed2ed2dac67740e6fd91b0f0bb5cc25dcf8
qfl <1093996321@qq.com>
2023-04-01 19:47:13 +0800

    second commit

--- IdeaProjects/git-demo/src/qfl/com/demo.java
+++ IdeaProjects/git-demo/src/qfl/com/demo.java
@@ -3,6 +3,7 @@ package qfl.com;
 public class demo {
     public static void main(String[] args){  // psvm
         System.out.print("first git test");
+        System.out.print("Second commit");
     }
 
 
